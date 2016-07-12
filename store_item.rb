949590b40922a0ff57019a00d140d8173a858482
Liam Willey <wwilleymc@gmail.com>
2016-07-11 20:11:40 -0700

    initial commit

---
+++ store_item.rb
@@ -0,0 +1,3 @@
+product_1 = {name: "tshirt", description: "clothing", brand: "wutang"}
+product_2 = {name: "shoes", description: "footwear", brand: "adidas"}
+product_3 = {name: "jacket", description: "outdoor wear", brand: "northface"}
